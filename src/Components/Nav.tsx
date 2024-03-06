070a6cd6d5f1f52b922e215bf765fc33d61bc244
daveanthonyc <daveanthonyc@gmail.com>
2024-03-06 20:21:03 +1100

    Nav: Added blog link

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -20,6 +20,7 @@ function Nav({textEnter, textLeave, backgroundState}: {textEnter: () => void, te
                     >
                     <a href="#" data-state={backgroundState} onMouseEnter={textEnter} onMouseLeave={textLeave} className="logo" title='home'>D<span className="clr-neutral-beige">C</span></a>
                     <div className="link-parent">
+                        <a href="https://david-chiang-blog.netlify.app/" data-state={backgroundState} target="_blank" onMouseEnter={textEnter} onMouseLeave={textLeave} className="link-block"><span className="fs-larger">B</span>log</a>
                         <a href="https://github.com/daveanthonyc" data-state={backgroundState} target="_blank" onMouseEnter={textEnter} onMouseLeave={textLeave} className="link-block"><span className="fs-larger">G</span>itHub</a>
                         <a href="mailto:daveanthonyc@gmail.com" title="email" data-state={backgroundState} onMouseEnter={textEnter} onMouseLeave={textLeave} className="link-block"><span className="fs-larger">C</span>ontact</a>
                     </div>
